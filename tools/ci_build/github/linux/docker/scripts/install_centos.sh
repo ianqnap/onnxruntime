477a220100e04775421dd75e140aaf88dd92af2e
Changming Sun <chasun@microsoft.com>
2019-10-04 14:16:07 -0700

    update

--- tools/ci_build/github/linux/docker/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/scripts/install_centos.sh
@@ -3,5 +3,7 @@ set -e
 
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-yum install -y redhat-lsb-core expat-devel libcurl-devel rpmdevtools tar unzip ccache curl gcc gcc-c++ zlib-devel make  python2-devel python3-devel python3-pip python3-numpy libunwind icu aria2 rsync python3-setuptools python3-wheel bzip2 git
+yum install -y redhat-lsb-core expat-devel libcurl-devel rpmdevtools tar unzip ccache curl zlib-devel make  python2-devel  libunwind icu aria2 rsync bzip2 git
 
+#If the /opt/python folder exists, we assume this is the manylinux docker image
+[ ! -d "/opt/python/cp35-cp35m" ] && yum install -y gcc gcc-c++ python3-devel python3-pip python3-numpy python3-setuptools python3-wheel
